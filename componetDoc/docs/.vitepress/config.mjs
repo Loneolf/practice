db30460adce59a194f86396a8196149161758ce6
loneolf <qinghaoqin@163.com>
2024-12-11 01:58:30 +0800

    config

---
+++ componetDoc/docs/.vitepress/config.mjs
@@ -0,0 +1,51 @@
+import { defineConfig } from "vitepress";
+import {
+	demoblockPlugin,
+	demoblockVitePlugin,
+} from "vitepress-theme-demoblock";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import path from "path";
+
+import sidebar from "./sidebar";
+
+export default defineConfig({
+	title: "文档库",
+	description: "组件库",
+	themeConfig: {
+		cleanUrls: true,
+		markdown: {
+			theme: { light: "github-light", dark: "github-dark" },
+			image: {
+				// 开启图片懒加载
+				lazyLoading: true,
+			},
+			config: (md) => {
+				md.use(demoblockPlugin, {
+					customClass: "demoblock-custom",
+				});
+			},
+		},
+
+		vite: {
+			plugins: [demoblockVitePlugin(), vueJsx()],
+			resolve: {
+				alias: {
+					"@c": path.resolve(__dirname, "../com"),
+				},
+			},
+		},
+
+		nav: [
+			{ text: "组件", link: "/componet/timeSelect", activeMatch: "/componet/" },
+			{ text: "工具", link: "/tool/filedsIndex", activeMatch: "/tool/" },
+			{ text: "文档", link: "/tooldoc/time", activeMatch: "/tooldoc/" },
+		],
+
+		sidebar,
+
+		//  开启搜索功能
+		search: {
+			provider: "local",
+		},
+	},
+});
---
+++ componetDoc/docs/.vitepress/sidebar.js
@@ -0,0 +1,42 @@
+export default {
+	"/componet/": [
+		{
+			items: [
+				{
+					text: "时间选择器",
+					link: "/componet/timeSelect",
+				},
+				{
+					text: "搜索组件",
+					link: "/componet/search",
+				},
+			],
+		},
+	],
+	"/tool/": [
+		{
+			// text: "Examples",
+			items: [
+				{
+					text: "字段下标获取",
+					link: "/tool/filedsIndex",
+				},
+				{
+					text: "字符串转换",
+					link: "/tool/stringTurn",
+				},
+			],
+		},
+	],
+	"/tooldoc/": [
+		{
+			// text: "方法文档",
+			items: [
+				{
+					text: "时间处理相关",
+					link: "/tooldoc/time",
+				},
+			],
+		},
+	],
+};
